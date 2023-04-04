43f471e8d91db49d8ba45dee66f2081b4ca251df
Stijn van den Berg <489789@student.fontys.nl>
2023-04-04 12:06:26 +0200

    Added support for Sass/Scss

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import DashboardCollection from "@/BLL/DashboardCollection";
 import Dashboard from "@/BLL/Dashboard";
 import Widget from "@/BLL/Widget";
-import "./style.css";
 
 import DashboardDALMemoryContext from "@/DAL/DashboardDALMemoryContext";
 import WidgetDALMemoryContext from "@/DAL/WidgetDALMemoryContext";
