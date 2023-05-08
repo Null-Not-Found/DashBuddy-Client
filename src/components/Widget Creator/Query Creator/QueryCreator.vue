0a25d54b553bd0346416d6d5da731f8204b2e359
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-05-08 13:07:48 +0200

    🎉 A match made in code heaven! Merged the title bar and graph picker into our new widget creator 🤝💻🔥

--- src/components/Widget Creator/Query Creator/QueryCreator.vue
+++ src/components/Widget Creator/Query Creator/QueryCreator.vue
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-<template><GraphTypePicker /></template>
+<template>
+  <TitleBar />
+  <GraphTypePicker />
+</template>
 
 <script setup lang="ts">
 import GraphTypePicker from "@/components/Widget Creator/Query Creator/GraphTypePicker.vue";
+import TitleBar from "@/components/Widget Creator/Query Creator/TitleBar.vue";
 </script>
-=======
-<template>Query Creator</template>
-
-<script setup lang="ts"></script>
->>>>>>> NULL-117-Make-Title-Bar
-
-<style scoped></style>
