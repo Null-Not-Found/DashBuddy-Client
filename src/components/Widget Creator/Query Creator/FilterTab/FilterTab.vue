7778aff4dc58cc8cfb2b552f8332d7de2b320bab
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-05-15 14:29:16 +0200

    📜 Behold the marvels of our time! The filter tab hath been adorned with a type picker or slider, reminiscent of the early 1700s, when the world was filled with wondrous contraptions and ingenious mechanisms ⌛🖋️⚙️

--- src/components/Widget Creator/Query Creator/FilterTab/FilterTab.vue
+++ src/components/Widget Creator/Query Creator/FilterTab/FilterTab.vue
@@ -1,15 +1,17 @@
 <template>
-  <RangeSlider :min="min" :max="max" :type="hello"></RangeSlider>
-  <ValuePicker :value="years"></ValuePicker>
+  <ValuePicker v-if="isStringArray" :value="value"></ValuePicker>
+  <RangeSlider v-else :min="value[0]" :max="value[1]"></RangeSlider>
 </template>
 
 <script setup lang="ts">
 import ValuePicker from "@/components/Widget Creator/Query Creator/FilterTab/ValuePicker.vue";
 import RangeSlider from "@/components/Widget Creator/Query Creator/FilterTab/RangeSlider.vue";
-const years: number[] = [2018, 2019, 2020, 2021, 2022];
-const min: number = 5;
-const max: number = 40;
-const hello: String = "title";
+
+const props = defineProps<{
+  value: String[] | number[];
+}>();
+
+let isStringArray: boolean = typeof props.value[0] === "string";
 </script>
 
 <style scoped></style>
