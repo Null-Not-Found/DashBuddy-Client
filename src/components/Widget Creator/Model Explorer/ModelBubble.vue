cdaafd270cc1874a915c5bce2d07e6b846eab23b
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-05-22 12:03:16 +0200

    🪄 Conjuring the Magic of Model Visualization! Created the captivating Model Bubble and List components, enchanting our application with mesmerizing visual representations 🌟✨🔮

---
+++ src/components/Widget Creator/Model Explorer/ModelBubble.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bubble">{{ modelName }}</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  id: Number,
+  modelName: String,
+});
+</script>
+
+<style scoped>
+.bubble {
+  display: inline-block;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: #345b8a;
+  color: white;
+}
+</style>
---
+++ src/components/Widget Creator/Model Explorer/ModelList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="models">
+    <ModelBubble
+      v-for="model in models"
+      :key="model.id"
+      :model-name="model.name"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import ModelBubble from "@/components/Widget Creator/Model Explorer/ModelBubble.vue";
+
+const props = defineProps({
+  models: Array,
+});
+</script>
+
+<style scoped>
+.models {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 0.5rem;
+}
+</style>
